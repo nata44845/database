ee3c59c82e2b65614e51ad15beb8910d7d6bcbe4
nata44845 <krppost@yandex.ru>
2023-07-29 14:00:07 +0700

    homework6

--- homework6.sql
+++ homework6.sql
@@ -1,17 +1,22 @@
+USE lesson_6;
+
 /*1. Создайте функцию, которая принимает кол-во сек и формат их в кол-во дней часов. 
 Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '*/
 
+drop procedure if exists get_days_hours;
 delimiter //
-create procedure get_days
+create procedure get_days_hours
 (
 	num int
 ) 
 begin 
- set @days=num/24+' дней'+num%24/60+' часов'+num%24%60/60+' минут';num/24+' дней';
- select @days;
+ declare days varchar(100);
+ set days=concat(FLOOR(num/(24*60*60)),' дней ',
+ FLOOR(num%(24*60*60)/(60*60)),' часов ',FLOOR(num%(60*60)/60),' минут ',num%60,' секунд');
+ select days;
 end //
 
-select get_days(123456);
+call get_days_hours(123456);
 
 /*2. Выведите только четные числа от 1 до 10. 
 Пример: 2,4,6,8,10 Данная промежуточная аттестация оценивается по системе "зачет" / "не зачет" 
